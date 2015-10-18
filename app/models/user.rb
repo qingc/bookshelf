3a2e5d8bc51694d7c46ab36e73641704e5d1263d
qingc <sandy.caiqing@gmail.com>
2015-10-18 23:40:16 +0800

    account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
 
 	#create an accessible attribute
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 	validates :name, presence: true, length: {maximum: 50}, uniqueness: true
 	validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}
 	has_secure_password
@@ -31,8 +33,29 @@ class User < ActiveRecord::Base
   	end
 
   	# Returns true if the given token matches the digest.
-  	def authenticated?(remember_token)
-  		return false if remember_digest.nil
-    	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  	def authenticated?(attribute, token)
+      digest = send("#{attribute}_digest")
+  		return false if digest.nil
+    	BCrypt::Password.new(digest).is_password?(token)
   	end
+
+    # activate account
+    def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+    end
+
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
+
+    private
+      def downcase_email
+        self.email = email.downcase
+      end
+
+      def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
+      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017131127) do
+ActiveRecord::Schema.define(version: 20151018050524) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
