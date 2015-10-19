b22f3dfb0d9564837bff5a48a34319f5678533d0
qingc <sandy.caiqing@gmail.com>
2015-10-19 23:02:06 +0800

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
   	# Returns true if the given token matches the digest.
   	def authenticated?(attribute, token)
       digest = send("#{attribute}_digest")
-  		return false if digest.nil
+  		return false if digest.nil?
     	BCrypt::Password.new(digest).is_password?(token)
   	end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018050524) do
+ActiveRecord::Schema.define(version: 20151019144123) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
