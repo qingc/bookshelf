65ded398fde230e01101d30da369933c4c1198d1
qingc <sandy.caiqing@gmail.com>
2015-10-19 13:58:49 +0800

    Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   resources :account_activations, only: [:edit]
+  resources :account_activations, only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
